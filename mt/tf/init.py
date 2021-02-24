66b713a82eceb856e5ac23e7b393bf2fc8fa536d
Minh-Tri Pham <pmtri80@gmail.com>
2021-02-24 11:19:24 +0000

    v0.0.4: bringing mobilenet_v3 to TF 2.3

--- mt/tf/init.py
+++ mt/tf/init.py
@@ -8,6 +8,10 @@ def init():
     import tensorflow
 
     if tensorflow.__version__.startswith('2.') and tensorflow.__version__ < '2.4':
+        # monkey-patch mobilenet_v3
+        from .keras_applications import mobilenet_v3
+        setattr(tensorflow.python.keras.applications, 'mobilenet_v3', mobilenet_v3)
+
         import h5py as _h5
         if _h5.__version__.startswith('3.'): # hack because h5py>=3.0.0 behaves differently from h5py<3.0.0
             from .keras_engine import hdf5_format

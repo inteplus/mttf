7bde3d5d2184fc49592f1429309773a753cfa6c0
Minh-Tri Pham <pmtri80@gmail.com>
2021-12-14 12:19:52 +0000

    v0.3: introducing 'gpus_in_tf_format' function

--- mt/tf/__init__.py
+++ mt/tf/__init__.py
@@ -1,5 +1,6 @@
 from .mttf_version import version as __mttf_version__
 from .init import init as mttf_init
+from .utils import gpus_in_tf_format
 from tensorflow import *
 import tensorflow.compiler as compiler
 import tensorflow.core as core

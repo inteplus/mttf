d4b797b85cd4e3376e1e3c13255616e4c71a7d78
Minh-Tri Pham <pmtri80@gmail.com>
2022-07-07 09:20:11 +0100

    stuff

--- mt/tfp/__init__.py
+++ mt/tfp/__init__.py
@@ -8,3 +8,4 @@ if V.parse(__version__) <= V.parse("0.17.0"):
     from .real_nvp import real_nvp_default_template
 
     bijectors.real_nvp.real_nvp_default_template = real_nvp_default_template
+    bijectors.real_nvp_default_template = real_nvp_default_template

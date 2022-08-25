a77e7fd111cc89e73f1d762b1a6ef03188f72d82
Minh-Tri <minhtri.pham@winnowsolutions.com>
2022-08-25 14:40:54 +0100

    introducing keras_applications.mobilevit

--- mt/tf/init.py
+++ mt/tf/init.py
@@ -4,6 +4,7 @@ from packaging import version
 
 __all__ = ['init']
 
+
 def init():
     '''Initialises tensorflow, monkey-patching if necessary.'''
 
@@ -16,26 +17,35 @@ def init():
         # monkey-patch mobilenet_v3
         from .keras_applications import mobilenet_v3
         if tf_ver < version.parse('2.4'):
-            setattr(tensorflow.python.keras.applications, 'mobilenet_v3', mobilenet_v3)
+            setattr(tensorflow.python.keras.applications,
+                    'mobilenet_v3', mobilenet_v3)
         setattr(tensorflow.keras.applications, 'mobilenet_v3', mobilenet_v3)
-        setattr(tensorflow.keras.applications, 'MobileNetV3Small', mobilenet_v3.MobileNetV3Small)
-        setattr(tensorflow.keras.applications, 'MobileNetV3Large', mobilenet_v3.MobileNetV3Large)
+        setattr(tensorflow.keras.applications,
+                'MobileNetV3Small', mobilenet_v3.MobileNetV3Small)
+        setattr(tensorflow.keras.applications,
+                'MobileNetV3Large', mobilenet_v3.MobileNetV3Large)
         sys.modules['tensorflow.python.keras.applications.mobilenet_v3'] = mobilenet_v3
 
         # monkey-patch CosineDecay
         from .keras_optimizers import lr_extra
-        setattr(tensorflow.keras.optimizers.schedules, 'CosineDecay', lr_extra.CosineDecay)
+        setattr(tensorflow.keras.optimizers.schedules,
+                'CosineDecay', lr_extra.CosineDecay)
         sys.modules['tensorflow.python.keras.optimizers.lr_extra'] = lr_extra
 
     # add mobilenet_v3_split module
-    from .keras_applications import mobilenet_v3_split
-    setattr(tensorflow.keras.applications, 'mobilenet_v3_split', mobilenet_v3_split)
+    from .keras_applications import mobilenet_v3_split, mobilevit
+    setattr(tensorflow.keras.applications,
+            'mobilenet_v3_split', mobilenet_v3_split)
+    setattr(tensorflow.keras.applications, 'mobilevit', mobilevit)
     sys.modules['tensorflow.keras.applications.mobilenet_v3_split'] = mobilenet_v3_split
-    setattr(tensorflow.keras.applications, 'MobileNetV3Split', mobilenet_v3_split.MobileNetV3Split)
+    sys.modules['tensorflow.keras.applications.mobilevit'] = mobilevit
+    setattr(tensorflow.keras.applications, 'MobileNetV3Split',
+            mobilenet_v3_split.MobileNetV3Split)
 
     if tf_ver < version.parse('2.5'):
         import h5py as _h5
-        if _h5.__version__.startswith('3.'): # hack because h5py>=3.0.0 behaves differently from h5py<3.0.0
+        # hack because h5py>=3.0.0 behaves differently from h5py<3.0.0
+        if _h5.__version__.startswith('3.'):
             from .keras_engine import hdf5_format
             tensorflow.python.keras.saving.hdf5_format = hdf5_format
             tensorflow.python.keras.engine.training.hdf5_format = hdf5_format
@@ -46,21 +56,31 @@ def init():
         from .keras_applications import efficientnet_v2
         if tf_ver < version.parse('2.4'):
             sys.modules['tensorflow.python.keras.applications.efficientnet_v2'] = efficientnet_v2
-            setattr(tensorflow.python.keras.applications, 'effiicientnet_v2', efficientnet_v2)
+            setattr(tensorflow.python.keras.applications,
+                    'effiicientnet_v2', efficientnet_v2)
         sys.modules['tensorflow.keras.applications.efficientnet_v2'] = efficientnet_v2
         if tf_ver >= version.parse('2.7'):
             sys.modules['keras.applications.efficientnet_v2'] = efficientnet_v2
             import keras
             setattr(keras.applications, 'effiicientnet_v2', efficientnet_v2)
-        setattr(tensorflow.keras.applications, 'efficientnet_v2', efficientnet_v2)
-        setattr(tensorflow.keras.applications, 'EfficientNetV2B0', efficientnet_v2.EfficientNetV2B0)
-        setattr(tensorflow.keras.applications, 'EfficientNetV2B1', efficientnet_v2.EfficientNetV2B1)
-        setattr(tensorflow.keras.applications, 'EfficientNetV2B2', efficientnet_v2.EfficientNetV2B2)
-        setattr(tensorflow.keras.applications, 'EfficientNetV2B3', efficientnet_v2.EfficientNetV2B3)
-        setattr(tensorflow.keras.applications, 'EfficientNetV2S', efficientnet_v2.EfficientNetV2S)
-        setattr(tensorflow.keras.applications, 'EfficientNetV2M', efficientnet_v2.EfficientNetV2M)
-        setattr(tensorflow.keras.applications, 'EfficientNetV2L', efficientnet_v2.EfficientNetV2L)
+        setattr(tensorflow.keras.applications,
+                'efficientnet_v2', efficientnet_v2)
+        setattr(tensorflow.keras.applications, 'EfficientNetV2B0',
+                efficientnet_v2.EfficientNetV2B0)
+        setattr(tensorflow.keras.applications, 'EfficientNetV2B1',
+                efficientnet_v2.EfficientNetV2B1)
+        setattr(tensorflow.keras.applications, 'EfficientNetV2B2',
+                efficientnet_v2.EfficientNetV2B2)
+        setattr(tensorflow.keras.applications, 'EfficientNetV2B3',
+                efficientnet_v2.EfficientNetV2B3)
+        setattr(tensorflow.keras.applications, 'EfficientNetV2S',
+                efficientnet_v2.EfficientNetV2S)
+        setattr(tensorflow.keras.applications, 'EfficientNetV2M',
+                efficientnet_v2.EfficientNetV2M)
+        setattr(tensorflow.keras.applications, 'EfficientNetV2L',
+                efficientnet_v2.EfficientNetV2L)
 
     return tensorflow
+
 
 init()
---
+++ mt/tf/keras_applications/mobilevit.py
@@ -0,0 +1,140 @@
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+# pylint: disable=invalid-name
+# pylint: disable=missing-function-docstring
+"""MobileViT model.
+
+Add the reference here. One for the paper, the other for keras page.
+"""
+
+
+import typing as tp
+
+from mt import tf
+
+
+try:
+    from keras import backend
+    from keras import models
+    from keras.layers import VersionAwareLayers
+    from keras.utils import data_utils, layer_utils
+except ImportError:
+    from tensorflow.python.keras import backend
+    from tensorflow.python.keras import models
+    from tensorflow.python.keras.layers import VersionAwareLayers
+    from tensorflow.python.keras.utils import data_utils, layer_utils
+
+
+layers = VersionAwareLayers()
+
+
+def conv_block(x, filters=16, kernel_size=3, strides=2):
+    conv_layer = layers.Conv2D(
+        filters, kernel_size, strides=strides, activation=tf.nn.swish, padding="same"
+    )
+    return conv_layer(x)
+
+
+# Reference: https://git.io/JKgtC
+
+
+def inverted_residual_block(x, expanded_channels, output_channels, strides=1):
+    m = layers.Conv2D(expanded_channels, 1, padding="same", use_bias=False)(x)
+    m = layers.BatchNormalization()(m)
+    m = tf.nn.swish(m)
+
+    if strides == 2:
+        m = layers.ZeroPadding2D(padding=imagenet_utils.correct_pad(m, 3))(m)
+    m = layers.DepthwiseConv2D(
+        3, strides=strides, padding="same" if strides == 1 else "valid", use_bias=False
+    )(m)
+    m = layers.BatchNormalization()(m)
+    m = tf.nn.swish(m)
+
+    m = layers.Conv2D(output_channels, 1, padding="same", use_bias=False)(m)
+    m = layers.BatchNormalization()(m)
+
+    if tf.math.equal(x.shape[-1], output_channels) and strides == 1:
+        return layers.Add()([m, x])
+    return m
+
+
+# Reference:
+# https://keras.io/examples/vision/image_classification_with_vision_transformer/
+
+
+def mlp(x, hidden_units, dropout_rate):
+    for units in hidden_units:
+        x = layers.Dense(units, activation=tf.nn.swish)(x)
+        x = layers.Dropout(dropout_rate)(x)
+    return x
+
+
+def transformer_block(x, transformer_layers, projection_dim, num_heads=2):
+    for _ in range(transformer_layers):
+        # Layer normalization 1.
+        x1 = layers.LayerNormalization(epsilon=1e-6)(x)
+        # Create a multi-head attention layer.
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+        )(x1, x1)
+        # Skip connection 1.
+        x2 = layers.Add()([attention_output, x])
+        # Layer normalization 2.
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        # MLP.
+        x3 = mlp(x3, hidden_units=[x.shape[-1] * 2,
+                 x.shape[-1]], dropout_rate=0.1,)
+        # Skip connection 2.
+        x = layers.Add()([x3, x2])
+
+    return x
+
+
+def mobilevit_block(x, num_blocks, projection_dim, strides=1):
+    # Local projection with convolutions.
+    local_features = conv_block(x, filters=projection_dim, strides=strides)
+    local_features = conv_block(
+        local_features, filters=projection_dim, kernel_size=1, strides=strides
+    )
+
+    # Unfold into patches and then pass through Transformers.
+    num_patches = int(
+        (local_features.shape[1] * local_features.shape[2]) / patch_size)
+    non_overlapping_patches = layers.Reshape((patch_size, num_patches, projection_dim))(
+        local_features
+    )
+    global_features = transformer_block(
+        non_overlapping_patches, num_blocks, projection_dim
+    )
+
+    # Fold into conv-like feature-maps.
+    folded_feature_map = layers.Reshape((*local_features.shape[1:-1], projection_dim))(
+        global_features
+    )
+
+    # Apply point-wise conv -> concatenate with the input features.
+    folded_feature_map = conv_block(
+        folded_feature_map, filters=x.shape[-1], kernel_size=1, strides=strides
+    )
+    local_global_features = layers.Concatenate(
+        axis=-1)([x, folded_feature_map])
+
+    # Fuse the local and global features using a convoluion layer.
+    local_global_features = conv_block(
+        local_global_features, filters=projection_dim, strides=strides
+    )
+
+    return local_global_features

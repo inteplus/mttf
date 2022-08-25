bac3b3e2a2355d30097e2d712d1297682438100c
Minh-Tri <minhtri.pham@winnowsolutions.com>
2022-08-25 12:52:51 +0100

    tf.utils.as_floatx

--- mt/tf/__init__.py
+++ mt/tf/__init__.py
@@ -1,10 +1,10 @@
 from .mttf_version import version as __mttf_version__
 from .init import init as mttf_init
-from .utils import gpus_in_tf_format
+from .utils import *
 from tensorflow import *
 import tensorflow.compiler as compiler
 import tensorflow.core as core
 import tensorflow.keras as keras
-import tensorflow.keras as lite
+import tensorflow.lite as lite
 import tensorflow.python as python
 import tensorflow.tools as tools

f168d2557289fce15be495227a8e9a8473bd6180
Minh-Tri Pham <pmtri80@gmail.com>
2023-06-16 06:53:17 +0100

    v0.31: introducing 'dmt_pipi.sh' and 'dmt_twineu.sh'

--- make_version.sh
+++ make_version.sh
@@ -14,7 +14,7 @@
 # ```
 
 MAJOR_VERSION=0
-MINOR_VERSION=30
+MINOR_VERSION=31
 
 VERSION_YEAR=`date -u +'%Y'`
 VERSION_MONTH=`date -u +'%m'`
--- setup.py
+++ setup.py
@@ -11,13 +11,15 @@ setup(
     packages=find_namespace_packages(include=["mt.*"]),
     install_requires=[
         # 'tensorflow', 'tensorflow-cpu' or 'tensorflow-gpu'
-        "mtbase>=4.5",  # just updating
+        "mtbase>=4.6",  # just updating
         "mtnet>=0.1",  # for some basic networking support
     ],
     scripts=[
         "scripts/wml_nexus.py",  # for accessing Winnow Nexus repo
         "scripts/wml_pipi.sh",  # for accessing Winnow Nexus repo
         "scripts/wml_twineu.sh",  # for accessing Winnow Nexus repo
+        "scripts/dmt_pipi.sh",  # for accessing Winnow Nexus repo, MT dev environment
+        "scripts/dmt_twineu.sh",  # for accessing Winnow Nexus repo, MT dev environment
         "scripts/twine_trusted.py",  # for the ability to disable ssl verification
     ],
     url="https://github.com/inteplus/mttf",

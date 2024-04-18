0e6e7273a98de099ac7c643c68367283f118e4f3
Minh-Tri <minhtri.pham@winnowsolutions.com>
2024-04-18 09:52:27 +0100

    raise minimum TF requirement to 2.8

--- mt/tf/init.py
+++ mt/tf/init.py
@@ -13,10 +13,10 @@ def init():
 
     tf_ver = version.parse(tensorflow.__version__)
 
-    if tf_ver < version.parse("2.5"):
+    if tf_ver < version.parse("2.8"):
         raise ImportError(
-            "The minimum TF version that mttf supports is 2.5. Your TF is {}. "
-            "Please upgrade.".format(tf_ver)
+            f"The minimum TF version that mttf supports is 2.8. Your TF is {tf_ver}. "
+            "Please upgrade."
         )
 
     # add mobilenet_v3_split module
@@ -37,53 +37,6 @@ def init():
     setattr(tensorflow.keras.layers, "Identical", Identical)
     setattr(tensorflow.keras.layers, "Upsize2D", Upsize2D)
     setattr(tensorflow.keras.layers, "Downsize2D", Downsize2D)
-
-    if tf_ver < version.parse("2.8"):
-        # monkey-patch efficientnet_v2
-        from .keras_applications import efficientnet_v2
-
-        sys.modules["tensorflow.keras.applications.efficientnet_v2"] = efficientnet_v2
-        if tf_ver >= version.parse("2.7"):
-            sys.modules["keras.applications.efficientnet_v2"] = efficientnet_v2
-            import keras
-
-            setattr(keras.applications, "effiicientnet_v2", efficientnet_v2)
-        setattr(tensorflow.keras.applications, "efficientnet_v2", efficientnet_v2)
-        setattr(
-            tensorflow.keras.applications,
-            "EfficientNetV2B0",
-            efficientnet_v2.EfficientNetV2B0,
-        )
-        setattr(
-            tensorflow.keras.applications,
-            "EfficientNetV2B1",
-            efficientnet_v2.EfficientNetV2B1,
-        )
-        setattr(
-            tensorflow.keras.applications,
-            "EfficientNetV2B2",
-            efficientnet_v2.EfficientNetV2B2,
-        )
-        setattr(
-            tensorflow.keras.applications,
-            "EfficientNetV2B3",
-            efficientnet_v2.EfficientNetV2B3,
-        )
-        setattr(
-            tensorflow.keras.applications,
-            "EfficientNetV2S",
-            efficientnet_v2.EfficientNetV2S,
-        )
-        setattr(
-            tensorflow.keras.applications,
-            "EfficientNetV2M",
-            efficientnet_v2.EfficientNetV2M,
-        )
-        setattr(
-            tensorflow.keras.applications,
-            "EfficientNetV2L",
-            efficientnet_v2.EfficientNetV2L,
-        )
 
     return tensorflow
 

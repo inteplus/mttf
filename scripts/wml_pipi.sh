bd37511a4904cf3c5f8de4d89c2eb73f1154bd5d
Minh-Tri Pham <pmtri80@gmail.com>
2025-04-19 10:05:08 +0100

    updated version.py to 1.0.9

--- scripts/wml_pipi.sh
+++ scripts/wml_pipi.sh
@@ -1,2 +1,2 @@
 #/!bin/bash
-wml_nexus.py pip3 install --trusted-host localhost --extra-index https://localhost:5443/repository/ml-py-repo/simple/ $@
+wml_nexus.py uv pip install --trusted-host localhost --extra-index https://localhost:5443/repository/ml-py-repo/simple/ $@

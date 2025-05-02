40a2fc065063f48960a41a66fb8b178ee6707093
Minh-Tri Pham <pmtri80@gmail.com>
2025-05-02 17:04:03 +0100

    updated version.py to 1.1.18

--- mt/tf/init.py
+++ mt/tf/init.py
@@ -13,30 +13,11 @@ def init():
 
     tf_ver = Version(tensorflow.__version__)
 
-    if tf_ver < Version("2.8"):
+    if tf_ver < Version("2.10"):
         raise ImportError(
-            f"The minimum TF version that mttf supports is 2.8. Your TF is {tf_ver}. "
+            f"The minimum TF version that mttf supports is 2.10. Your TF is {tf_ver}. "
             "Please upgrade."
         )
-
-    # add mobilenet_v3_split module
-    from mt.keras.applications import mobilenet_v3_split, mobilevit
-
-    setattr(tensorflow.keras.applications, "mobilenet_v3_split", mobilenet_v3_split)
-    setattr(tensorflow.keras.applications, "mobilevit", mobilevit)
-    sys.modules["tensorflow.keras.applications.mobilenet_v3_split"] = mobilenet_v3_split
-    sys.modules["tensorflow.keras.applications.mobilevit"] = mobilevit
-    setattr(
-        tensorflow.keras.applications,
-        "MobileNetV3Split",
-        mobilenet_v3_split.MobileNetV3Split,
-    )
-
-    from mt.keras.layers import Identical, Upsize2D, Downsize2D
-
-    setattr(tensorflow.keras.layers, "Identical", Identical)
-    setattr(tensorflow.keras.layers, "Upsize2D", Upsize2D)
-    setattr(tensorflow.keras.layers, "Downsize2D", Downsize2D)
 
     return tensorflow
 

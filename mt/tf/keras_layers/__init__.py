b0bd7afe592ff33044267540c41634ff42fc4711
Minh-Tri Pham <pmtri80@gmail.com>
2025-05-02 11:20:10 +0100

    make mt.tf.keras_layers deprecated

--- mt/tf/keras_layers/__init__.py
+++ mt/tf/keras_layers/__init__.py
@@ -1,31 +1,5 @@
-from .identical import *
-from .floor import *
-from .var_regularizer import *
-from .simple_mha import *
-from .image_sizing import *
-from .counter import Counter
-from .normed_conv2d import NormedConv2D
-from .utils import *
+from mt.logg import logger
 
+logger.warn_module_move("mt.tf.keras_layers", "mt.keras.layers")
 
-__api__ = [
-    "Identical",
-    "Floor",
-    "VarianceRegularizer",
-    "SimpleMHA2D",
-    "MHAPool2D",
-    "DUCLayer",
-    "Downsize2D",
-    "Upsize2D",
-    "Downsize2D_V2",
-    "Upsize2D_V2",
-    "Downsize2D_V3",
-    "Downsize2D_V4",
-    "DownsizeX2D",
-    "UpsizeX2D",
-    "DownsizeY2D",
-    "UpsizeY2D",
-    "Counter",
-    "conv2d",
-    "dense2d",
-]
+from mt.keras.layers import *

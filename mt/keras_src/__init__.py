4c99547d3fbf363056c192a18f9e86ebb01a22c8
Minh-Tri Pham <pmtri80@gmail.com>
2025-06-06 07:26:23 +0100

    updated version.py to 1.2.13

--- mt/keras_src/__init__.py
+++ mt/keras_src/__init__.py
@@ -1,3 +1,5 @@
+from packaging.version import Version
+
 from .base import keras_version, keras_source
 
 if keras_source == "tf_keras":
@@ -8,3 +10,7 @@ elif keras_source == "tensorflow.keras":
     from tensorflow.keras import *
 else:
     raise ImportError(f"Unknown value '{keras_source}' for variable 'keras_source'.")
+
+d_modelFileFormats = {"H5": ".h5", "TF": ".tf"}
+if Version(keras_version) >= Version("2.15"):
+    d_modelFileFormats["Keras"] = ".keras"

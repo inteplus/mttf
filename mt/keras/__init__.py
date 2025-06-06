75e566421bce7cad5f73effe17ebe7b8780fb7ea
Minh-Tri Pham <pmtri80@gmail.com>
2025-06-06 06:58:20 +0100

    updated version.py to 1.2.10

--- mt/keras/__init__.py
+++ mt/keras/__init__.py
@@ -1,4 +1,8 @@
 """MT package that represents the working Keras 2 from the system."""
 
 from mt.keras_src import *
-from mt.keras_src import layers_src as layers, applications_src as applications
+from mt.keras_src import (
+    constraints_src as constraints,
+    layers_src as layers,
+    applications_src as applications,
+)

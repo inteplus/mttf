7118a095afa54c62dee53d99961f42da4a18f6f0
Minh-Tri Pham <pmtri80@gmail.com>
2025-06-10 04:24:55 +0100

    fixed a bug at CenterAround code

--- mt/keras_src/constraints_src/center_around.py
+++ mt/keras_src/constraints_src/center_around.py
@@ -2,7 +2,7 @@ from .. import constraints
 
 
 class CenterAround(constraints.Constraint):
-    """Constrains weight tensors to be centered around `ref_value`."""
+    """Constrains the last axis to have values centered around `ref_value`."""
 
     def __init__(self, ref_value: float = 0.0):
         self.ref_value = ref_value
@@ -10,8 +10,10 @@ class CenterAround(constraints.Constraint):
     def __call__(self, w):
         import tensorflow as tf
 
-        mean = tf.reduce_mean(w)
-        return w - (mean - self.ref_value)
+        mean = tf.reduce_mean(w, axis=-1, keepdims=True)
+        ref_mean = mean - self.ref_value
+        ref_mean = tf.expand_dims(ref_mean, -1)
+        return w - ref_mean
 
     def get_config(self):
         return {"ref_value": self.ref_value}

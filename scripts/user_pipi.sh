d87c2b5663f00529793d8ac5d93cc289d8698ff4
Minh-Tri Pham <pmtri80@gmail.com>
2026-01-24 18:56:02 +0000

    (bug-fix commit by MT)

--- scripts/user_pipi.sh
+++ scripts/user_pipi.sh
@@ -1,7 +1,7 @@
 #/!bin/bash
 pipi_url="https://localhost:5443/repository/${USER}-pypi-dev/simple/"
 if [ $(id -u) -ne 0 ]; then
-  echo "WARNING: As of 2026-01-24, you need to activate a virtual environment (e.g. uv venv) before pip-installing wml packages."
+  echo "WARNING: As of 2026-01-24, you need to create a virtual environment (e.g. uv venv) before pip-installing wml packages."
   wml_nexus.py uv pip install --allow-insecure-host localhost --index $pipi_url --index-strategy unsafe-best-match $@
 else
   wml_nexus.py uv pip install -p /usr/bin/python3 --system --break-system-packages --prerelease allow --allow-insecure-host localhost --index $pipi_url --index-strategy unsafe-best-match $@

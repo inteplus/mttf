fb2a4cc780bd92b4436ef192902f9ebac6502f32
Minh-Tri Pham <pmtri80@gmail.com>
2026-01-24 17:12:50 +0000

    update user_pipi.sh

--- scripts/user_pipi.sh
+++ scripts/user_pipi.sh
@@ -1,8 +1,8 @@
 #/!bin/bash
 pipi_url="https://localhost:5443/repository/${USER}-pypi-dev/simple/"
 if [ $(id -u) -ne 0 ]; then
-  echo "WARNING: As of 2025-04-20, it is not safe to install wml packages locally."
-  wml_nexus.py pip3 install --trusted-host localhost --extra-index $pipi_url --upgrade $@
+  echo "WARNING: As of 2026-01-24, you need to activate a virtual environment (e.g. uv venv) before pip-installing wml packages."
+  wml_nexus.py uv pip install --allow-insecure-host localhost --index $pipi_url --index-strategy unsafe-best-match $@
 else
   wml_nexus.py uv pip install -p /usr/bin/python3 --system --break-system-packages --prerelease allow --allow-insecure-host localhost --index $pipi_url --index-strategy unsafe-best-match $@
 fi

53c292df62dabe3d8ed7d6359471e36417da7fe5
Minh-Tri Pham <pmtri80@gmail.com>
2026-01-24 07:59:49 +0000

    updated version.py to 1.3.3

--- scripts/user_build_package_and_upload_to_nexus.sh
+++ scripts/user_build_package_and_upload_to_nexus.sh
@@ -10,13 +10,13 @@ if git rev-parse --git-dir > /dev/null 2>&1; then
     echo "===== Building the Python package residing at ${GIT_REPO_PATH} ====="
     cd ${GIT_REPO_PATH}
     uv build  # previously: ./setup.py bdist_wheel
-    echo "===== Installing the Python package, may need sudo privilege ====="
+    echo "===== Installing the Python package ====="
     WHEEL_FILE=`ls -t1 dist | head -n 1`
     PACKAGE_NAME=`echo "${WHEEL_FILE}" | cut -d'-' -f1`
     echo "Package name: ${PACKAGE_NAME}"
     echo "Wheel to install: ${WHEEL_FILE}"
-    sudo uv pip uninstall --system --break-system-packages ${PACKAGE_NAME}
-    sudo user_pipi.sh -U dist/${WHEEL_FILE}
+    uv pip uninstall ${PACKAGE_NAME}
+    user_pipi.sh -U dist/${WHEEL_FILE}
     echo "===== Uploading Python package to Winnow's Nexus server ====="
     user_twineu.sh dist/${WHEEL_FILE}
     cd ${CURR_PATH}

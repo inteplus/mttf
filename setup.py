23ee36ad0c1467b9d955ed2c2fe127cc3f680e2f
Minh-Tri Pham <pmtri80@gmail.com>
2026-06-09 09:32:54 +0000

    updated version.py to 1.3.17

--- setup.py
+++ setup.py
@@ -21,13 +21,13 @@ setup(
         "Documentation": "https://mtdoc.readthedocs.io/en/latest/mt.tf/mt.tf.html",
         "Source Code": "https://github.com/inteplus/mttf",
     },
-    setup_requires=["setuptools-git-versioning<2"],
+    setup_requires=["setuptools-git-versioning>=3,<4"],
     setuptools_git_versioning={
         "enabled": True,
         "version_file": VERSION_FILE,
         "count_commits_from_version_file": True,
         "template": "{tag}",
-        "dev_template": "{tag}.dev{ccount}+{branch}",
-        "dirty_template": "{tag}.post{ccount}",
+        "dev_template": "{tag}",
+        "dirty_template": "{tag}",
     },
 )

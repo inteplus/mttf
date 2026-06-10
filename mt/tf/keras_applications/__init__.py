c0c6d03b981f2af478dbe30bdc890a2e271577c2
Minh-Tri Pham <pmtri80@gmail.com>
2026-06-10 09:10:03 +0000

    no more tf.keras

--- mt/tf/keras_applications/__init__.py
+++
@@ -1,5 +0,0 @@
-from mt.logg import logger
-
-logger.warn_module_move("mt.tf.keras_applications", "mt.keras.applications")
-
-from mt.keras.applications import *
--- mt/tf/keras_layers/__init__.py
+++
@@ -1,5 +0,0 @@
-from mt.logg import logger
-
-logger.warn_module_move("mt.tf.keras_layers", "mt.keras.layers")
-
-from mt.keras.layers import *
